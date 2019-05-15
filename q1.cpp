b1c35854a678662b18812741445f365f09771ce1
candr006 <candr006@ucr.edu>
2019-05-15 16:18:28 -0700

    added file

---
+++ q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int SPScore(string x, string y){
+	
+	return 2;
+}
+
+vector<string> threeSeqAlign(vector<string> s1,vector<string> s2, vector<string> s3){
+	vector<string> test;
+	return test;
+}
+	
+
+int main(){
+	
+	
+	
+}
