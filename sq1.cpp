68b8377d83085230dddea598e1e6959b729ee003
candr006 <candr006@ucr.edu>
2019-05-23 19:59:53 -0700

    add version with arrays instead of vectors

---
+++ sq1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <iterator>
+#include <string>   
+
+using namespace std;
+
+//set the constant values here 
+int match=5;
+int mismtch=-4;
+int indel=-8;
+
+
+int main(int argc, char** argv){
+	
+	//sequence 1
+	ifstream ifs(argv[1]);
+	string s1_t;
+	s1_t.assign( (istreambuf_iterator<char>(ifs) ),
+					(istreambuf_iterator<char>()));
+    char s1[s1_t.length() + 1]; 
+	strcpy(s1, s1_t.c_str());
+	
+	//sequence 2
+	ifstream ifs2(argv[2]);
+	string s2_t;
+	s2_t.assign( (istreambuf_iterator<char>(ifs2) ),
+					(istreambuf_iterator<char>()));
+    char s2[s2_t.length()+1]; 
+	strcpy(s2, s2_t.c_str());
+	
+	//sequence 3
+	ifstream ifs3(argv[3]);
+	string s3_t;
+	s3_t.assign( (istreambuf_iterator<char>(ifs3) ),
+					(istreambuf_iterator<char>()));
+    char s3[s3_t.length()+1]; 
+	strcpy(s3, s3_t.c_str());
+	
+}
